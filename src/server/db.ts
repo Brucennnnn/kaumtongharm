6340d439616ceeabd1ecc1b67531e8a16e201874
bruce <bruceoio@hotmail.com>
2024-03-20 21:52:23 +0700

    change to correct prisma client

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,17 +1,18 @@
 import { PrismaClient } from "@prisma/client";
-
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
 import { env } from "@ktm/env";
 
+const libsql = createClient({
+  url: `${env.DATABASE_URL}`,
+  authToken: `${env.DATABASE_AUTH}`,
+});
+const adapter = new PrismaLibSQL(libsql);
 const createPrismaClient = () =>
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    adapter: process.env.NODE_ENV === "production" ? adapter : undefined,
   });
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof createPrismaClient> | undefined;
-};
-
-export const db = globalForPrisma.prisma ?? createPrismaClient();
-
-if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+export const db = createPrismaClient();
