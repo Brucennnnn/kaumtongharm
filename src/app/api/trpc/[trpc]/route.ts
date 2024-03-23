45141107f5011a0639b6c7b6316096b4adc51b7e
bruce <bruceoio@hotmail.com>
2024-03-23 17:45:42 +0700

    feat: add session in use client context

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -4,14 +4,17 @@ import { type NextRequest } from "next/server";
 import { env } from "@ktm/env";
 import { appRouter } from "@ktm/server/api/root";
 import { createTRPCContext } from "@ktm/server/api/trpc";
+import { validateRequest } from "@ktm/server/api/auth";
 
 /**
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
  * handling a HTTP request (e.g. when you make requests from Client Components).
  */
 const createContext = async (req: NextRequest) => {
+  const { session } = await validateRequest();
   return createTRPCContext({
     headers: req.headers,
+    session: session,
   });
 };
 
@@ -25,7 +28,7 @@ const handler = (req: NextRequest) =>
       env.NODE_ENV === "development"
         ? ({ path, error }) => {
             console.error(
-              `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
+              `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
             );
           }
         : undefined,
