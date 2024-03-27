790bbe040cdafd5cd77b248a056c5c63d56979df
bruce <bruceoio@hotmail.com>
2024-03-28 00:28:29 +0700

    fix

--- src/server/db.ts
+++ src/server/db.ts
@@ -3,13 +3,20 @@ import { PrismaLibSQL } from "@prisma/adapter-libsql";
 import { createClient } from "@libsql/client";
 import { env } from "@ktm/env";
 
-const libsql = createClient({
-  url: `${env.DATABASE_URL}`,
-  authToken: `${env.DATABASE_AUTH}`,
-});
+let libsql: ReturnType<typeof createClient> | undefined;
+if (env.NODE_ENV === "production") {
+  libsql = createClient({
+    url: `${env.DATABASE_URL}`,
+    authToken: `${env.DATABASE_AUTH}`,
+  });
+} else {
+  libsql = createClient({
+    url: `file:prisma/dev.db`,
+  });
+}
 
 const adapter = new PrismaLibSQL(libsql);
 export const db = new PrismaClient({
   log: env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  adapter: process.env.NODE_ENV === "production" ? adapter : null,
+  adapter: adapter,
 });
