fcd761a61d619264af3a61854a7b0eddde7a9e53
Punnawat Lohanuit <punnawat58000@gmail.com>
2024-03-27 20:26:47 +0700

    feat: room list

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,6 +1,46 @@
 import { signup } from "@ktm/action/auth";
 import { validateRequest } from "@ktm/server/api/auth";
+import { type player, type gameRoom } from "../interfaces";
+import GameRoomList from "../_components/GameRoomList";
+
+const p1: player = {
+  name: "Nicepun",
+  score: 0,
+  myWord: null,
+};
+
+const p2: player = {
+  name: "Bruce",
+  score: 0,
+  myWord: null,
+};
+
+const p3: player = {
+  name: "Kim",
+  score: 0,
+  myWord: null,
+};
+
+const room1: gameRoom = {
+  name: "This is room1 name",
+  status: 3,
+  maxRound: 5,
+  players: [p1, p2],
+  maxPlayer: 10,
+  details: "for thai people",
+};
+
+const room2: gameRoom = {
+  name: "This is room2 name long long long long long long long long",
+  status: 0,
+  maxRound: 7,
+  players: [p3],
+  maxPlayer: 4,
+  details: "for thai people",
+};
+
 export default async function Page() {
+  // const [selectedRoom, setSelectedRoom];
   const { user } = await validateRequest();
   if (user) {
     console.log("this user", user);
@@ -17,6 +57,7 @@ export default async function Page() {
       {/*   <br /> */}
       {/*   <button>Continue</button> */}
       {/* </form> */}
+      <GameRoomList allRooms={[room1, room2]} />
     </>
   );
 }
