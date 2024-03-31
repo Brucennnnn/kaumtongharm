d094ca2c6fe378a1b39800da7f6ef89c783d8f8f
Punnawat Lohanuit <punnawat58000@gmail.com>
2024-03-31 17:41:41 +0700

    fix: cardwrapper

--- src/app/gameroom/_components/CardWrapper.tsx
+++ src/app/gameroom/_components/CardWrapper.tsx
@@ -1,10 +1,4 @@
-<<<<<<<< HEAD:src/app/gameroom/_components/GameWrapper.tsx
-export default function GameWrapper({
-|||||||| parent of 0b3b725 (fix: folder name):src/app/chat/_components/ChatWrapper.tsx
-export default function ChatWrapper({
-========
 export default function CardWrapper({
->>>>>>>> 0b3b725 (fix: folder name):src/app/game-room-list/_components/CardWrapper.tsx
   leftside,
   rightside,
 }: {
@@ -12,7 +6,7 @@ export default function CardWrapper({
   rightside: React.ReactNode;
 }) {
   return (
-    <div className="scrollbar-hide flex h-full w-full min-w-[240px] max-w-[900px] flex-col overflow-hidden overflow-y-scroll rounded-lg border-2 border-stroke  bg-background lg:max-h-[600px] lg:flex-row">
+    <div className="flex h-full w-full min-w-[240px] max-w-[900px] flex-col overflow-hidden overflow-y-scroll rounded-lg border-2 border-stroke bg-background  scrollbar-hide lg:max-h-[600px] lg:flex-row">
       <div className="flex h-full min-h-[250px] w-full bg-main p-4 lg:max-h-full">
         {leftside}
       </div>
