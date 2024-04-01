9772bafadc8d215c9af846b2462144075bf23220
bruce <bruceoio@hotmail.com>
2024-04-02 00:10:20 +0700

    feat:update

--- src/server/api/routers/gameroom.ts
+++ src/server/api/routers/gameroom.ts
@@ -1,8 +1,12 @@
-import { createTRPCRouter, publicProcedure } from "@ktm/server/api/trpc";
+import {
+  createTRPCRouter,
+  publicProcedure,
+  userProcedure,
+} from "@ktm/server/api/trpc";
 import { z } from "zod";
 import { Prisma, type KaumTongHarm } from "@prisma/client";
 export const gameRoomRouter = createTRPCRouter({
-  createGameRoom: publicProcedure
+  createGameRoom: userProcedure
     .input(
       z.object({
         roomName: z.string().min(1),
@@ -19,6 +23,7 @@ export const gameRoomRouter = createTRPCRouter({
             maxPlayers: input.maxPlayers,
             rounds: input.rounds,
             description: input.description,
+            hostId: ctx.session.userId,
             chat: {
               create: {},
             },
