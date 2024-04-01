1da5b18cccf578eed3dafe2deeef68f9a021fd20
KKhimmoon <khin292546@gmail.com>
2024-04-01 19:05:47 +0700

    add bg

--- src/app/gameroom/page.tsx
+++ src/app/gameroom/page.tsx
@@ -4,7 +4,7 @@ import LeftSideRoomList from "./_components/LeftSideRoomList";
 import RightSideCreateGame from "./_components/RightSideCreateGame";
 export default function Page() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
+    <div className="bg-bgImage flex min-h-screen items-center justify-center bg-cover bg-center">
       <GameWrapper
         leftside={<LeftSideRoomList />}
         rightside={<RightSideCreateGame />}
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,7 +8,7 @@ import LoginTab from "./_components/LoginTab";
 import SignUpTab from "./_components/SignUpTab";
 export default function Page() {
   return (
-    <div className="flex min-h-screen w-screen items-center justify-center bg-background">
+    <div className="bg-bgImage flex min-h-screen w-screen items-center justify-center bg-cover bg-center">
       <Tabs defaultValue="login" className="flex w-fit flex-col pb-10">
         <TabsList className="justify-end">
           <TabsTrigger className="data-[state=active]:bg-main" value="login">
