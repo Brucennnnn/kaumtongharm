3ba63e9aa7c51e9f5266caad7f7375f44cf267c0
Punnawat Lohanuit <punnawat58000@gmail.com>
2024-04-04 02:45:24 +0700

    fix: search

--- src/app/gameroom/page.tsx
+++ src/app/gameroom/page.tsx
@@ -1,30 +1,23 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import GameRoomList from "./_components/GameRoomList";
 import { type gameRoom } from "./interfaces";
 import CardWrapper from "./_components/GameWrapper";
 import RightSideCreateGame from "./_components/RightSideCreateGame";
 import GameRoomDetails from "./_components/GameRoomDetails";
-import { api } from "@ktm/trpc/react";
 import LogoutButton from "../_components/LogoutButton";
 
 export default function Page() {
-  const [searchString, setSearchString] = useState("");
   const [selectedRoom, setSelectedRoom] = useState<gameRoom | null>(null);
-
-  const { isSuccess, data } = api.gameRoom.getGameRoomsByFilter.useQuery({
-    // searchQuery: searchString,
-  });
-  if (!data) return <></>;
   return (
     <div className="flex h-screen min-h-fit items-center justify-center bg-bgImage p-4">
       <CardWrapper
         leftside={
           <GameRoomList
-            setSearchString={setSearchString}
+            // setSearchString={setSearchString}
             selectedroom={selectedRoom}
             setSelectedRoom={setSelectedRoom}
-            allGameRoom={data}
+            // allGameRoom={data}
           />
         }
         rightside={
