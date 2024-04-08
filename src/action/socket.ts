1f9871edf4f960fdee01b35a46f96c396cb5248d
bruce <bruceoio@hotmail.com>
2024-04-08 18:33:16 +0700

    feat:update

--- src/action/socket.ts
+++ src/action/socket.ts
@@ -3,7 +3,5 @@ import { env } from '@ktm/env';
 
 import { io } from 'socket.io-client';
 
-export const socket = io(env.NEXT_PUBLIC_SOCKET_API, {
-  transports: ['websocket'],
-});
+export const socket = io(env.NEXT_PUBLIC_SOCKET_API);
 socket.connect();
