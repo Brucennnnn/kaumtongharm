50820ae74cfbdf1ff018456a2f47ad28d1f2bc45
bruce <bruceoio@hotmail.com>
2024-04-08 18:53:19 +0700

    feat:update

--- src/action/socket.ts
+++ src/action/socket.ts
@@ -4,6 +4,6 @@ import { env } from '@ktm/env';
 import { io } from 'socket.io-client';
 
 export const socket = io(env.NEXT_PUBLIC_SOCKET_API, {
-  transports: ['polling'],
+  transports: ['websocket'],
 });
 socket.connect();
